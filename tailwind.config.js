771ed94ab7a91b0c2882014ba88d3d4ede717460
K G Shreya <96654167+Shreya2012p@users.noreply.github.com>
2024-09-11 17:01:24 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // All JS, JSX, TS, and TSX files in the src folder
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
